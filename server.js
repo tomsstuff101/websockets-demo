498af52c0648199351cc54290d81ffac839e66e4
CN105 <cn105@CN0105.local>
2019-10-07 15:51:10 +0100

    added io.on connection

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const path = require('path')
 const http = require('http')
-const socketio = require('socketio')
+const socketio = require('socket.io')
 
 
-const publicDir = path.join(__dirname, "./piublic")
+const publicDir = path.join(__dirname, "./public")
 
 const app = express()
 
@@ -13,6 +13,14 @@ app.use(express.static(publicDir))
 const server = http.createServer(app) // this is the key difference for websockets
 
 const io = socketio(server) // create an instance of socket.io
+
+
+
+io.on("connection", () => {
+    console.log('New WebSocket Connection')
+})
+
+
 
 server.listen(3001, () => {  // this is then changed from app.listen to server.listen
     console.log('The server is running on port 3001..')
